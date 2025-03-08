ec25f041aee51252f2dbc5ffa0608a0cd67e2d46
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-03-08 15:08:30 +0545

    callback to promise

---
+++ async/callBackToPromis.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+const cbToPromise = new Promise((resolve, reject)=>{
+    fs.readFile("data2.txt", 'utf-8', (error, data)=>{
+        if(error) return reject(error);
+        
+        resolve(data)
+    })
+
+});
+
+cbToPromise
+.then((data)=>{
+    console.log(data)
+})
+.catch(()=>{
+    console.log(error)
+})
