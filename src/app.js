430259921014d0a43a814972d6e1abe1e13e4b96
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-03-09 19:50:40 +0545

    express route

--- src/app.js
+++ src/app.js
@@ -2,7 +2,29 @@ import express from 'express';
 
 const app = express();
 
-app.
+app.get("/", (req, res)=>{
+    res.send("<h1>Home Page</h1>");
+    // res.status(401).json({message: "Hello"});
+})
+
+app.get("/about", (req, res)=>{
+    res.send("<h1>About Page</h1>");
+})
+
+app.post("/about", (req, res)=>{
+    res.send("<h1>Create data on about page</h1>");
+})
+
+app.get("/products", (req, res)=>{
+    res.send("<h1>Product page</h1>");
+})
+
+app.get("/products/:id", (req, res)=>{
+    const id = req.params.id;
+    res.send(`<h1>Product by id: ${id}</h1>`);
+})
+
+
 
 app.listen(5000,()=>{
     console.log("Server started at port 5000...")
