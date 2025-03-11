54240e04f0f106c2e36f3217ce64af99ebb6a75e
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-03-11 22:28:16 +0545

    database connection with env variable

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 async function connectDB() {
     // database connection code
     try {
-        const status = await mongoose.connect("mongodb://localhost:27017/");
+        const status = await mongoose.connect(process.env.MONGODB_URL);
         console.log(`MongoDB connected: ${status.connection.host}`);
     } catch (error) {
         console.log(`DB error ${error.message}`);
