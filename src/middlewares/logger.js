bede2cb7868ded02a5abfcead785f314cb2fd9b4
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-03-18 21:28:49 +0545

    litlle changes in logger

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,7 +1,8 @@
-const logger = (res, req, next)=> {
-
-    console.log("Hello from logger");
-
+const logger = (req, res, next)=> {
+    console.log(`Method: ${req.method} & Url: ${req.originalUrl}`);
+    if(req.method==='PATCH'){
+        res.status(405).send("Patch method not allowed")
+    }
     next();
 
 }
