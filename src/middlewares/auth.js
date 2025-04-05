329e77bb61b53363956bca8913e796b5fa8874f7
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-04-05 21:06:23 +0545

    verify jwtToken

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,5 @@
+import { verifyJWT } from "../utils/jwt.js";
+
 const auth = (req, res, next)=>{
     console.log("Auth middleware called");
 
@@ -5,6 +7,17 @@ const auth = (req, res, next)=>{
     if(!cookie) return res.status(401).send("User not authenticated!");
     
     const authToken = cookie.split("=")[1];
+    
+    verifyJWT(authToken)
+    .then((data)=>{
+        req.user = data;
+
+        next();
+    })
+    .catch(()=>{
+        res.status(400).send("Invalid token")
+    });
+    
     console.log("Middleware",authToken);
 
     next();
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,4 +5,13 @@ const createJWT = (data)=>{
 
 }
 
-export default createJWT;
+const verifyJWT = async (authToken)=>{
+    return await new Promise((resolve, reject)=>{
+        jwt.verify(authToken, process.env.JWT_SECRET, (error, data)=>{
+            if(error) return reject(error);
+            resolve(data);
+        })
+    })
+}
+
+export {createJWT, verifyJWT};
