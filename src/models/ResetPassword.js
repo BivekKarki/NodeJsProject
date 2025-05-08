807aef588cae1529036ea3538e9979dcd0255ade
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-05-08 15:50:29 +0545

    model for reset pasword

---
+++ src/models/ResetPassword.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    expiresAt: {
+        type: Date,
+        default: Date.now() + 300000, //5 minutes
+    },
+    isUsed: {
+        type: Boolean,
+        default: false,
+    }
+})
