fe16f7e4bd14cc8a7d803a8ffe0eab6569af5eed
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-05-23 13:21:23 +0545

    Init payment functionality

---
+++ src/models/Payment.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema({
+    amount:{type: Number, required: true},
+    paymentMethod: {
+        type: String,
+        required: true,
+        enum: ["cash", "card", "online"]
+    },
+    status: {
+        type: String,
+        enum: ["pending", "completed", "failed"],
+        default: "pending",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    order: {
+        type: mongoose.Types.ObjectId,
+        ref: "Order",
+        required: true,
+    },
+    transacrionId: String,
+
+})
+
+const model = mongoose.model("Payment", paymentSchema);
+
+export default model;
