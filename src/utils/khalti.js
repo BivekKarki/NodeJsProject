dd82fbf289f44f657821b2b0cf194a8c8a9232fc
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-05-26 21:20:24 +0545

    khalti function complete

--- src/utils/khalti.js
+++ src/utils/khalti.js
@@ -1,38 +1,49 @@
 import axios from 'axios';
 
-const payViaKhalti = ()=> {
-    let data = JSON.stringify({
-  "return_url": "https://fbb.com.np/",
-  "website_url": "https://fbb.com.np/",
-  "amount": 299999,
-  "purchase_order_id": "test12",
-  "purchase_order_name": "test",
-  "customer_info": {
-    "name": "Khalti Bahadur",
-    "email": "example@gmail.com",
-    "phone": "9800000123"
-  }
-});
+const payViaKhalti = async (data)=> {
 
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'https://dev.khalti.com/api/v2/epayment/initiate/',
-  headers: { 
-    'Authorization': 'Key 2f19dcd49e744f5399250b2956f82e79', 
-    'Content-Type': 'application/json', 
-    'Cookie': 'sessioncookie=!8DJZci7Neq8Vqkudi8GHrcsPaIwS0YhoevS16cYuC2T0jNeNq6cCSEJoQBlBlP9GyPwwxK02H17yoQ=='
-  },
-  data : data
-};
+    const requestBody = {
+        return_url: data.returnUrl,
+        website_url: data.websiteUrl,
+        amount: data.amount,
+        purchase_order_id: data.orderId,
+        purchase_order_name: data.orderName,
+        customer_info: data.customerInfo
+    } 
 
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+   
+
+// let config = {
+//   method: 'post',
+//   maxBodyLength: Infinity,
+//   url: 'https://dev.khalti.com/api/v2/epayment/initiate/',
+//   headers: { 
+//     'Authorization': 'Key 2f19dcd49e744f5399250b2956f82e79', 
+//     'Content-Type': 'application/json', 
+//     'Cookie': 'sessioncookie=!8DJZci7Neq8Vqkudi8GHrcsPaIwS0YhoevS16cYuC2T0jNeNq6cCSEJoQBlBlP9GyPwwxK02H17yoQ=='
+//   },
+//   data : data
+// };
+
+// axios.request(config)
+// .then((response) => {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
+
+
+
+const response = await axios.post(
+    process.env.KHALTI_URL, 
+    requestBody, 
+    {
+        Authorization: `Key ${process.env.KHALTI_API_KEY}`, 
+        'Content-Type': 'application/json',
+    }
+)
+return response.data;
 
 }
 
