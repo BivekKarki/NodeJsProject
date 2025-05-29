f1461f0ae6331195cfd749973789e99e20d9099e
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-05-29 20:48:17 +0545

    routes for home and product page

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -1,9 +1,12 @@
+import productService from "../services/productService.js"
+
 const homePage = (req, res)=> {
     res.render("home", {username: "Bivek"})
 }
 
-const productsPage = (req, res)=> {
-    res.render("products")
+const productsPage = async (req, res)=> {
+    const products = await productService.getAllProducts({sort:"{}"});
+    res.render("products", {products})
 }
 
 export { homePage, productsPage }
