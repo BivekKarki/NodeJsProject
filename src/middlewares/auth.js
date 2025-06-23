53c2bfe80ca1942fa3c8b6db937942398ee12fe6
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-06-23 20:31:10 +0545

    feat: check authToken from localStorage and cookie

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,11 +3,18 @@ import { verifyJWT } from "../utils/jwt.js";
 const auth = (req, res, next)=>{
     console.log("Auth middleware called");
 
-    const cookie = req.headers.cookie;
-    if(!cookie) return res.status(401).send("User not authenticated!");
-    
-    const authToken = cookie.split("=")[1];
-    
+    const authHeader = req.headers.authorization;
+
+    let authToken;
+
+    if(authHeader && authHeader.startsWith("Bearer ")){
+        authToken = authHeader.split(" ")[1];
+    }else{
+        const cookie = req.headers.cookie;
+        if(!cookie) return res.status(401).send("User not authenticated!");
+        authToken = cookie.split("=")[1];
+    }
+
     verifyJWT(authToken)
     .then((data)=>{
         req.user = data;
